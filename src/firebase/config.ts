2eceb231a3d11c7d3fd14330c08fb44e6bc17a69
Mohammed Nooh <mdnooh49@gmail.com>
2026-01-31 01:09:15 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-4063063908-148ad",
-  "appId": "1:276702333801:web:4422d5eb3aa06205e98bf8",
-  "apiKey": "AIzaSyAOwQGD4jma-eZGVCvsGfEV3rWyeIuUNVE",
-  "authDomain": "studio-4063063908-148ad.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "276702333801"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
